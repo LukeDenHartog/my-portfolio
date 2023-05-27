f7eeb867b8dfd3111395d42f77f96a5ecb5b5f65
Luke <121516961+Afrozez@users.noreply.github.com>
2023-05-26 21:01:47 -0700

    added height to about me line and a picture of myself

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,20 @@ readMoreButton = document.getElementById('read-more');
 readMoreButton.addEventListener("click", showMore);
 readLessButton = document.getElementById('read-less')
 readLessButton.addEventListener("click", showLess)
-
+aboutMeLine = document.querySelector('#first-line')
 
 function showMore() {
     secondParagraph.classList.remove("hide-display");
     readMoreButton.classList.add("hide-display");
     readLessButton.classList.remove("hide-display")
+    aboutMeLine.classList.remove('straight-line-divider')
+    aboutMeLine.classList.add('straight-line-divider-500')
 }
 
 function showLess() {
     readLessButton.classList.add("hide-display");
     secondParagraph.classList.add("hide-display")
     readMoreButton.classList.remove("hide-display");
+    aboutMeLine.classList.remove('straight-line-divider-500')
+    aboutMeLine.classList.add('straight-line-divider')
 }
