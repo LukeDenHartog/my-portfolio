a9c6effd835dcb0a544f01e1f7c546f402db71b0
Luke <121516961+Afrozez@users.noreply.github.com>
2023-05-27 20:20:45 -0700

    added styles to media query to remove padding/banner

--- assets/script.js
+++ assets/script.js
@@ -5,17 +5,19 @@ readLessButton = document.getElementById('read-less')
 readLessButton.addEventListener("click", showLess)
 aboutMeLine = document.querySelector('#first-line')
 
+
+
 function showMore() {
     secondParagraph.classList.remove("hide-display");
     readMoreButton.classList.add("hide-display");
-    readLessButton.classList.remove("hide-display")
-    aboutMeLine.classList.remove('straight-line-divider')
-    aboutMeLine.classList.add('straight-line-divider-500')
+    readLessButton.classList.remove("hide-display");
+    aboutMeLine.classList.remove('straight-line-divider');
+    aboutMeLine.classList.add('straight-line-divider-500');
 }
 
 function showLess() {
     readLessButton.classList.add("hide-display");
-    secondParagraph.classList.add("hide-display")
+    secondParagraph.classList.add("hide-display");
     readMoreButton.classList.remove("hide-display");
     aboutMeLine.classList.remove('straight-line-divider-500')
     aboutMeLine.classList.add('straight-line-divider')
